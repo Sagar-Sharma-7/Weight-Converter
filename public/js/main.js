d805aae5bad70147475a66f1e7ae6e4cdf7f4669
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2021-03-12 17:26:34 +0530

    changed according to v1.0.1

--- public/js/main.js
+++ public/js/main.js
@@ -1,69 +1,128 @@
+const mainHeading = document.querySelector("#mainHeading");
+const btn = document.querySelectorAll(".btn");
+const inputField = document.querySelector(".userWeight");
+const hiddenItems = document.querySelectorAll("#hide");
+const unitName = document.querySelector(".unitName");
+const resultContainer = document.querySelector(".resultContainer");
+const resultBox = document.querySelectorAll(".resultBox");
+const kilograms = document.querySelector(".kilograms");
+const grams = document.querySelector(".grams");
+const pounds = document.querySelector(".pounds");
+const ounces = document.querySelector(".ounces");
+const calculateBtn = document.querySelector(".calculateBtn");
+const backBtn = document.querySelector(".backBtn");
 
 
+// static content
+mainHeading.textContent = `Type your weight in any unit.`;
+kilograms.textContent = `Kilograms`;
+grams.textContent = `Grams`;
+pounds.textContent = `Pounds`;
+ounces.textContent = `Ounces`;
+calculateBtn.textContent = `Calculate`;
+backBtn.textContent = `Go Back`;
 
-// kilograms
-kg = () => {
-    document.getElementById("gm").style.display = 'none';
-    document.getElementById("pd").style.display = 'none';
-    document.getElementById("oc").style.display = 'none';
-    document.getElementById("reset-1").style.display = 'block';
-    document.getElementById("inputField").style.display = 'flex';
-    document.getElementById("basic-addon2").innerHTML = "Kilograms";
 
-    weightConverter1 = (x) => {
-      document.getElementById("result1").innerHTML = "Grams: " + x * 1000 + "<br>" + "Pounds: " + x * 2.2046 + "<br>" + "Ounces: " + x * 35.274;
-    
-    }
+// click function of every button
+btn.forEach((button) => {
+    button.addEventListener("click", () => {
+        console.log("Button Clicked");
+        
+        unitName.innerHTML = button.name;
+        
+        // hiding all the buttons
+        btn.forEach((hideMe) => {
+            hideMe.style.display = "none";
+        });
+        
+        // showing the hidden elements
+        hiddenItems.forEach((items) => {
+            resultContainer.style.display = "flex";
+            items.style.display = "block";
+        });
+    });
+});
+
+// white background color of result boxes
+whiteBG = () => {
+    resultBox.forEach((resultBoxWhite) => {
+        resultBoxWhite.style.backgroundColor = "#fff";
+    });
 };
+
+// kilogram
+kilograms.addEventListener("click", () => {
+    mainHeading.textContent = `Type your weight in kilograms`;
+
+    calculateBtn.addEventListener("click", () => {
+        const userWeightKg = inputField.value;
+
+        whiteBG();
+        // showing result
+        // grams
+        resultBox[0].innerHTML = `${userWeightKg * 1000} gm`;
+        //pounds
+        resultBox[1].innerHTML = `${userWeightKg * 2.20462} lbs`;
+        //ounces
+        resultBox[2].innerHTML = `${userWeightKg * 35.274} oz`;
+    });
+});
 
 // grams
-gm = () => {
-    document.getElementById("kg").style.display = 'none';
-    document.getElementById("pd").style.display = 'none';
-    document.getElementById("oc").style.display = 'none';
-    document.getElementById("reset-1").style.display = 'block';
-    document.getElementById("inputField").style.display = 'flex';
-    document.getElementById("basic-addon2").innerHTML = "Grams";
+grams.addEventListener("click", () => {
+    mainHeading.textContent = `Type your weight in grams`;
 
-    weightConverter1 = (x) => {
-        document.getElementById("result1").innerHTML = "Kilograms: " + x / 1000 + "<br>" + "Pounds: " + x * 0.0022046 + "<br>" + "Ounces: " + x * 0.035274;
-      
-      }
-};
-
-// Pounds
-pd = () => {
-    document.getElementById("gm").style.display = 'none';
-    document.getElementById("kg").style.display = 'none';
-    document.getElementById("oc").style.display = 'none';
-    document.getElementById("reset-1").style.display = 'block';
-    document.getElementById("inputField").style.display = 'flex';
-    document.getElementById("basic-addon2").innerHTML = "Pounds";
-
-    weightConverter1 = (x) => {
-        document.getElementById("result1").innerHTML = "Kilograms: " + x / 2.2046 + "<br>" + "Grams: " + x / 0.0022046 + "<br>" + "Ounces: " + x * 16;
-      
-      }
-};
-
-// Ounces
-oc = () => {
-    document.getElementById("gm").style.display = 'none';
-    document.getElementById("kg").style.display = 'none';
-    document.getElementById("pd").style.display = 'none';
-    document.getElementById("reset-1").style.display = 'block';
-    document.getElementById("inputField").style.display = 'flex';
-    document.getElementById("basic-addon2").innerHTML = "Ounces";
-
-    weightConverter1 = (x) => {
-        document.getElementById("result1").innerHTML = "Kilograms: " + x / 35.275 + "<br>" + "Grams: " + x / 0.035274 + "<br>" + "Pounds: " + x * 0.0625;
-      
-      }
-};
+    calculateBtn.addEventListener("click", () => {
+        const userWeightGm = inputField.value;
 
 
+        whiteBG();
+        // showing result
+        // kg
+        resultBox[0].innerHTML = `${userWeightGm / 1000} kg`;
+        // pounds
+        resultBox[1].innerHTML = `${userWeightGm / 454} lbs`;
+        // ounces
+        resultBox[2].innerHTML = `${userWeightGm / 28.35} oz`;
+    });
+});
+
+// pounds
+pounds.addEventListener("click", () => {
+    mainHeading.textContent = `Type your weight in pounds`;
+
+    calculateBtn.addEventListener("click", () => {
+        const userWeightLbs = inputField.value;
+
+        whiteBG();
+        // showing result
+        // kg
+        resultBox[0].innerHTML = `${userWeightLbs / 2.20462} kg`;
+        //gm
+        resultBox[1].innerHTML = `${userWeightLbs * 454} lbs`;
+        //ounces
+        resultBox[2].innerHTML = `${userWeightLbs * 16} oz`;
+    });
+});
+
+ounces.addEventListener("click", () => {
+    mainHeading.textContent = `Type your weight in ounces`;
+
+    calculateBtn.addEventListener("click", () => {
+        const userWeightOz = inputField.value;
+
+        whiteBG();
+        // showing result
+        // kg
+        resultBox[0].innerHTML = `${userWeightOz / 35.274} kg`;
+        // gm
+        resultBox[1].innerHTML = `${userWeightOz * 28.3} gm`;
+        // pounds
+        resultBox[2].innerHTML = `${userWeightOz / 16} lbs`
+    });
+});
 
 
-reset = () => {
-   location.reload();
-}
+backBtn.addEventListener("click", () => {
+    location.reload();
+});
